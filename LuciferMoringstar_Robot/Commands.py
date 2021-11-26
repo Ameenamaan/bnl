94d24028b9eac972a0d9cdc8465951cf3582cb73
Ameenamaan <88217467+Ameenamaan@users.noreply.github.com>
2021-11-26 19:25:01 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -116,19 +116,19 @@ async def start(bot, message):
             START_MSG,
             parse_mode="Markdown",
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup
+            reply_markup=InlineKeyboardMarkup(
              
         [[
         InlineKeyboardButton('➕️ Add Me To Your Group ➕️', url='http://t.me/Autofilter_TGR_Bot?startgroup=true'),
-      InlineKeyboardButton("ℹ️ Help", callback_data="help"),
-     InlineKeyboardButton('🤴 Dev', url ='https://t.me/Ameen_Ameen_12'),           
-   InlineKeyboardButton("About", callback_data="about"),
-                ],[
-                   InlineKeyboardButton('♻️ Group', url='https://t.me/HDmoviePp'),
-InlineKeyboardButton('⭕️ Channel', url ='https://t.me/HDmovieDp')
-    ],[
+        InlineKeyboardButton("ℹ️ Help", callback_data="help"),
+        InlineKeyboardButton('🤴 Dev', url ='https://t.me/Ameen_Ameen_12'),           
+        InlineKeyboardButton("About", callback_data="about"),
+        ],[
+        InlineKeyboardButton('♻️ Group', url='https://t.me/HDmoviePp'),
+        InlineKeyboardButton('⭕️ Channel', url ='https://t.me/HDmovieDp')
+        ],[
         InlineKeyboardButton('🕵️ Any Doubts 🕵️', url='https://t.me/Ameen_Ameen_12')
-    ]]
+        ]]
             )
         )
         StopPropagation
